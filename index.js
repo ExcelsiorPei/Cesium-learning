67fa9b5411957a049637954cb67024d7c5d96ae2
ExcelsiorPei <2500578997@qq.com>
2020-07-07 15:57:42 +0800

    enter

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 /// <reference path="index.d.ts"/>
+var viewer = new Cesium.Viewer("cesiumContainer");
+        var entity = viewer.entities.add({
+            position: Cesium.Cartesian3.fromDegrees(100, 60),
+            model: {
+                uri: 'test002.gltf'
+            }
+        });
+        viewer.trackedEntity = entity;
